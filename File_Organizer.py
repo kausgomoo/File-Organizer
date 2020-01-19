66f6fb4788a5754df38770977c3398975bf61ea8
kausgomoo <60049489+kausgomoo@users.noreply.github.com>
2020-01-18 19:45:36 -0500

    Update File_Organizer.py

---
+++ File_Organizer.py
@@ -0,0 +1,13 @@
+from pip._vendor.distlib.compat import raw_input
+#from tkinter import filedialog
+
+#set user parameters and determine file pathing
+print("Welcome to blank. Please choose your file settings. (i.e. where you want your external files to go)")
+type = ""
+
+type == raw_input("Please select the type of files you would like to store (please separate with spaces")
+type.split()
+
+
+
+    #if re.match(type):
